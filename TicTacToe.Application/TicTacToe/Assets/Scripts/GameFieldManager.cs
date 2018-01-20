1fdcab720a23ed15c4984827eca409bf34292bea
Nikola <nik@webtema.ru>
2018-01-20 21:56:27 +0200

    add unity project

---
+++ TicTacToe.Application/TicTacToe/Assets/Scripts/GameFieldManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFieldManager : Singleton<GameFieldManager>
+{
+    public Transform root;
+    public Transform fieldTile;
+    public Camera mainCamera;
+
+    public float tileMargin = 0.1f;
+    public int tileCount = 20;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int x = 0; x < tileCount; x++)
+        {
+            for (int y = 0; y < tileCount; y++)
+            {
+                SpawnFieldTile(new Vector3(x + x * tileMargin, 1, y + y * tileMargin));
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static GameObject SpawnFieldTile(Vector3 position)
+    {
+        var obj = PoolManager.SpawnObject(Instance.fieldTile.gameObject, position, Quaternion.identity);
+
+        obj.transform.parent = Instance.root;
+        obj.SetActive(true);
+
+        return obj;
+    }
+}
