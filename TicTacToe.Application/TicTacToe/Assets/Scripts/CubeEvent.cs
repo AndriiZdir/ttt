46f0fd220681a4629b668685f1759b13a4aefe0e
User <User@WIN-LVBQ9F2UU77>
2018-01-30 14:18:37 +0200

    Cube

---
+++ TicTacToe.Application/TicTacToe/Assets/Scripts/CubeEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeEvent : MonoBehaviour
+{
+
+    private MeshRenderer _thisVisibility = null;
+    private Transform _thisTransform = null;
+
+    public float TravelTime = 3f;
+    
+    void Awake()
+    {
+        _thisVisibility = GetComponent<MeshRenderer>();
+        _thisTransform = GetComponent<Transform>();
+    }
+
+    public void OnTapEnable()
+    {
+        StartCoroutine(OnTapEnableCorutine());
+    }
+
+    IEnumerator OnTapEnableCorutine()
+   {
+       
+        Debug.Log("Tap registered!");
+        _thisTransform.Translate(new Vector3(0,5,0));
+       
+       yield return new WaitForSeconds(3);
+      _thisVisibility.gameObject.SetActive(false);
+      yield break;
+
+   }
+}
